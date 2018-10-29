35b09ac82efb25388581eb0841e769f87735b294
ptsouth97 <ptsouth97@gmail.com>
2018-10-29 11:27:53 -0400

    updated files

--- README.md
+++ README.md
@@ -14,13 +14,14 @@ The application was tested using Python 3.6 running on Ubuntu and relies on:
 2. To run from the main application from the command line, use 'chmod +x binomial_distribution.py' 
 3. In the working directory, there is a file name 'data.csv' containing the experimental data
 4. Executing the application will load this data into a pandas dataframe
-5. The columns of 'Partial Overlap' and 'Prior' will be sliced and their averages computed
+5. The columns of 'Partial Overlap' and 'Precedes' will be sliced and their averages computed
 6. The application will generate a probability mass function (pmf) based on 10,000 simulations of the binomial distribution for each case ('Partial Overlap', 'Precedes', and 'Chance')
 7. The pmfs for 'Partial Overlap and Chance' and 'Precedes and Chance' are graphed using a matplotlib histogram
 8. A confidence interval of 95% (alpha  = 0.05) was chosen to establish statistical significane 
 9. A two-tailed test was used, so each tail required a p-value < 0.025  (alpha / 2 = 0.025)
 10. The p-value was calculated by summing the area under the 'Chance' curve up the experimentally determined averages
 11. This p-value was annotated on the plot
+12. The poster folder contains the Latex file and associated files for generating the conference poster
 
 ## Future plans
 * Re-doing experimental design to account for potential flaws in the original study
---
+++ app_fitnesstracker.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import matplotlib.pyplot as plt	
+import numpy as np
+
+
+def main():
+	''' main function'''
+
+	# Read the data file and assign to a pandas dataframe
+	file_name = 'data.csv'
+	df = pd.read_csv(file_name)
+
+	# Slice the column containing 'Partial Overlap' values
+	overlap = df.loc[:,'Partial Overlap']
+
+	# Compute metrics of interest
+	overlap_len = len(overlap)
+	overlap_avg = overlap.mean()
+	overlap_sum = overlap.sum()
+
+	# Slice the column containing 'Precedes' values
+	precedes = df.loc[:,'Precedes']
+
+	# Compute metrics of interest
+	precedes_len = len(precedes)
+	precedes_avg = precedes.mean()
+	precedes_sum = precedes.sum()
+
+	# Calculate chance sum (how many occurences expected by chance)
+	chance_sum = len(precedes) * 0.5
+
+	# Take 10,000 samples out of the binomial distribution for each case
+	overlapping = np.random.binomial(overlap_len, overlap_avg, size=10000)
+	preceding = np.random.binomial(precedes_len, precedes_avg, size=10000)	
+	chance = np.random.binomial(precedes_len, 0.5, size=10000)
+
+	# Call function to plot the probability mass function for OVERLAP
+	pmf(overlapping, overlap_sum, chance, chance_sum, 'Overlaps' )
+
+	# Call function to plot the probability mass function for PRECEDES
+	pmf(preceding, precedes_sum, chance, chance_sum, 'Precedes')
+
+
+def pmf(condition_df, condition_sum, chance_df, chance_sum, condition):
+	''' Plots probability mass function along with p-value'''
+
+	# Compute bin edges: bins
+	bins = np.arange(0, max(chance_df)+1.5) - 0.5
+
+	# Generate chance histogram
+	values, bins, _ = plt.hist(chance_df, normed=True, bins=bins, label='Chance')
+
+	# Generate condition histogram
+	_ = plt.hist(condition_df, normed=True, bins=bins, alpha=0.5, label=condition)
+
+	calc_bins = np.arange(min(chance_df), max(chance_df))
+
+	# Calculate p-value/2 (two-tail test) for when the value is greater or less than the avg
+	if condition_sum > chance_sum:
+		p = sum(values[(condition_sum):(max(calc_bins))])/sum(values)
+
+	else:
+		p = sum(values[0:(condition_sum+1)])/sum(values)
+
+	# Set margins
+	_ = plt.margins(0.02)
+
+	# Label axes
+	_ = plt.xlabel('Number of successes')
+	_ = plt.ylabel('probability')
+	_ = plt.title('PMF for Elevated HR ' +condition+ ' Problem Behavior')
+	_ = plt.axvline(condition_df.mean(), color='k', linestyle='dashed', linewidth=1)
+	_ = plt.axvline(chance_df.mean(), color='k', linestyle='dashed', linewidth=1)
+	_ = plt.annotate('p/2='+str(p), (0,0.08))
+
+	# Make a legend
+	plt.legend(loc='upper left')
+	
+	# Show the plot
+	name = condition+'.png'
+	plt.savefig(name)
+	plt.show()
+	plt.close()
+
+
+if __name__ == '__main__':
+	main()
